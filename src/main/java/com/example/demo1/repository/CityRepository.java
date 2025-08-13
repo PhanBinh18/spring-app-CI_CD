d7bd3fe452a25c8be6245fd28c13ec279bddf3a3
Pbinh <pbinhf@gmail.com>
2025-08-13 15:52:51 +0700

    feat: create CityRepository interface

--- src/main/java/com/example/demo1/repository/CityRepository.java
+++ src/main/java/com/example/demo1/repository/CityRepository.java
@@ -1,4 +1,15 @@
 package com.example.demo1.repository;
 
-public interface CityRepository {
+
+import com.example.demo1.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+    // Tìm các thành phố có 'name' bắt đầu bằng từ khóa, không phân biệt hoa thường
+    // Chúng ta cũng có thể giới hạn số lượng kết quả, ví dụ lấy 20 thành phố đầu tiên
+    List<City> findByNameStartingWithIgnoreCase(String name);
 }
